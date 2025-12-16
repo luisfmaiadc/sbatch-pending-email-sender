865dae97186cf7cab63c6aed218b408882bd71ba
Luis Felipe Maia <lf.mcosta23@gmail.com>
2025-12-16 19:50:23 -0300

    Estrutura inicial do projeto.

---
+++ src/test/java/com/portfolio/luisfmdc/sbatch_pending_email_sender/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sbatch_pending_email_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
